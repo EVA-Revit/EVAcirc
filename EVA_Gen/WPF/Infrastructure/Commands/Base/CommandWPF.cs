c7be2b77565f4d10990655a8ac8b1503d81350c1
shipovskiy <shipovskiy@WS-141>
2021-03-23 16:14:29 +0300

    Главный класс команды

---
+++ EVA_Gen/WPF/Infrastructure/Commands/Base/CommandWPF.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EVA_Gen.WPF.Infrastructure.Commands.Base
+{
+    internal abstract class CommandWPF : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            //Отдаем управление событием классу CommandManager WPF генерирует это событие во всех командах
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+
+
+        public abstract void Execute(object parameter);
+
+    }
+}
---
+++ EVA_Gen/WPF/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,32 @@
+﻿using EVA_Gen.WPF.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVA_Gen.WPF.Infrastructure.Commands
+{
+    internal class LambdaCommand : CommandWPF
+    {
+        //поля
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        //Конструктор
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            //Получение двух делигатов
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute)); //исключение если нет ссылки на делигат
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true; // Если нет делигата всё равно выполнять команду
+
+
+
+        public override void Execute(object parameter)
+        {
+            _Execute(parameter);
+        }
+    }
+}
